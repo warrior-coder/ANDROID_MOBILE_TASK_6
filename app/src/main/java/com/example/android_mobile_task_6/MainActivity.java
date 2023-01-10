0cd394bc9a60e35f1f86570de6dba488ed85ed4a
warrior-coder <gudkou2003@gmail.com>
2023-01-10 21:53:55 +0300

    Add HTTP picture loading

--- app/src/main/java/com/example/android_mobile_task_6/MainActivity.java
+++ app/src/main/java/com/example/android_mobile_task_6/MainActivity.java
@@ -1,14 +1,131 @@
 package com.example.android_mobile_task_6;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+//import android.support.v7.app.ActionBarActivity;/
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressDialog progressDialog;
+    private Bitmap bitmap = null;
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkInternetConnection();
+                downloadImage("https://www.tutorialspoint.com/green/images/logo.png");
+            }
+        });
     }
+
+    private void downloadImage(String urlStr) {
+        progressDialog = ProgressDialog.show(this, "", "Downloading Image from " + urlStr);
+        final String url = urlStr;
+
+        new Thread() {
+            public void run() {
+                InputStream in = null;
+
+                Message msg = Message.obtain();
+                msg.what = 1;
+
+                try {
+                    in = openHttpConnection(url);
+                    bitmap = BitmapFactory.decodeStream(in);
+                    Bundle b = new Bundle();
+                    b.putParcelable("bitmap", bitmap);
+                    msg.setData(b);
+                    in.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                messageHandler.sendMessage(msg);
+            }
+        }.start();
+    }
+
+    private InputStream openHttpConnection(String urlStr) {
+        InputStream in = null;
+        int resCode = -1;
+
+        try {
+            URL url = new URL(urlStr);
+            URLConnection urlConn = url.openConnection();
+
+            if (!(urlConn instanceof HttpURLConnection)) {
+                throw new IOException("URL is not an Http URL");
+            }
+
+            HttpURLConnection httpConn = (HttpURLConnection) urlConn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            resCode = httpConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return in;
+    }
+
+    @SuppressLint("HandlerLeak")
+    private final Handler messageHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            img.setImageBitmap((Bitmap) (msg.getData().getParcelable("bitmap")));
+            progressDialog.dismiss();
+        }
+    };
+
+    private void checkInternetConnection() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager connect = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        // Check for network connections
+        if (connect.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTED ||
+                connect.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connect.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTING ||
+                connect.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.CONNECTED) {
+            Toast.makeText(this, " Connected ", Toast.LENGTH_LONG).show();
+        } else if (connect.getNetworkInfo(0).getState() == android.net.NetworkInfo.State.DISCONNECTED ||
+                connect.getNetworkInfo(1).getState() == android.net.NetworkInfo.State.DISCONNECTED) {
+            Toast.makeText(this, " Not Connected ", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
